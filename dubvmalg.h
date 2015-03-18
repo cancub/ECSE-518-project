2f9bf32e3855e306f1bd87587bd706802921cf8e
cancub <cancub@live.com>
2015-03-18 14:35:48 -0400

    fully functional pagerank

---
+++ dubvmalg.h
@@ -0,0 +1,15 @@
+#ifndef DUBVMALG_H_INCLUDED
+#define DUBVMALG_H_INCLUDED
+
+#include "dubarrays.h"
+
+void alphaxtimesyTplusA(double alpha, struct DubArray * x, struct DubArray * y, struct DubArray * A);
+double dotproduct(struct DubArray * x, struct DubArray * y);
+void alphaxplusy_y(double alpha, struct DubArray * x, struct DubArray * y);
+void scale(struct DubArray * x, double scale);
+struct DubArray alphaATtimesx(double ALPHA, struct DubArray * A,struct DubArray * x);
+double differce_vector_length(struct DubArray * x1,struct DubArray * x0);
+struct DubArray makecopy(struct DubArray * a);
+double L1_difference(struct DubArray * x1, struct DubArray * x0);
+
+#endif
---
+++ twodarrays.h
@@ -0,0 +1,23 @@
+#ifndef TWODARRAYS_H_INCLUDED
+#define TWODARRAYS_H_INCLUDED
+
+#include <stddef.h>
+
+struct Array
+{
+	int * array;
+	size_t size;
+};
+
+struct TwoDArray
+{
+	struct Array * array;
+	size_t size;
+};
+
+void add_array(struct TwoDArray * a);
+void add_element(struct TwoDArray * a, int n, int value);
+void construct_2DArray(struct TwoDArray * a);
+void print_2DArray(struct TwoDArray * a);
+
+#endif
