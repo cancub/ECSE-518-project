94f6a4229466f56af60cefba0c91f0fa77a99eed
cancub <cancub@live.com>
2015-03-19 18:43:05 -0400

    building upon webcrawler

--- webcrawler.c
+++ webcrawler.c
@@ -14,13 +14,16 @@ int main()
 	// char web_address[BUFFLEN] = "wget -q http://www.github.com -O - |     tr \"\\t\\r\\n'\" '   \"' |     grep -i -o '<a[^>]\\+href[ ]*=[ \\t]*\"\\(ht\\|f\\)tps\\?:[^\"]\\+\"' |     sed -e 's/^.*\"\\([^\"]\\+\\)\".*$/\\1/g' > test.txt"; 
 	// system(web_address);
 
-	char test[] = "abscda.as.5165";
-	char * s;
-	s = strchr(test,'.');
+	char test[] = "http://www.mcgill.ca";
+	char * s = strchr(test,'/');
+	char *t = strchr(s+2*sizeof(char),'.');
 
-	int distance = s-test;
 
-	printf("%d\n", distance);
+	//produces actual value location, e.g. test[6]
+	int last_slash = s-test+1;
+	int first_dot = t - test;
+
+	printf("%d %d\n", last_slash, first_dot);
 
 
 
