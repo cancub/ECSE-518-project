bb9f53ed95f02acf3466d26d00a02d1f4b0fc431
cancub <cancub@live.com>
2015-03-20 11:16:30 -0400

    progressing towards functional webcrawler

--- webcrawler.c
+++ webcrawler.c
@@ -3,48 +3,301 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define BUFFLEN 2500
 
 //http://stackoverflow.com/questions/298510/how-to-get-the-current-directory-in-a-c-program
 
+char * flip_url(char * url);
+char * fix_errors(char * url);
+
+
 int main()
 {
+
+	char * flipped;
+
+	char output[200];
+
+	int i;
 
 	// char web_address[BUFFLEN] = "wget -q http://www.github.com -O - |     tr \"\\t\\r\\n'\" '   \"' |     grep -i -o '<a[^>]\\+href[ ]*=[ \\t]*\"\\(ht\\|f\\)tps\\?:[^\"]\\+\"' |     sed -e 's/^.*\"\\([^\"]\\+\\)\".*$/\\1/g' > test.txt"; 
 	// system(web_address);
 
-	char test[] = "http://www.mcgill.ca";
-	char * s = strchr(test,'/');
-	char *t = strchr(s+2*sizeof(char),'.');
+	// char url[] = "https://github.com";
 
-
-	//produces actual value location, e.g. test[6]
-	int last_slash = s-test+1;
-	int first_dot = t - test;
-
-	printf("%d %d\n", last_slash, first_dot);
+	// printf("%s\n", flip_url(url));	
 
 
 
 
 
-	// FILE * ifp = fopen("test.txt","r");
+	FILE * ifp = fopen("test.txt","r");
+	FILE * ofp = fopen("final.txt","a");
 
-	// char output[50];
 
-	// fscanf(ifp,"%s", output);
 
-	// char * strA = "Blahblahblah", * strB = "123", strC[50];
-	// int x = 4;
-	// strncpy(strC,strA,x);
-	// strC[x] = '\0';
-	// strcat(strC,strB);
-	// strcat(strC,strA+x);
-	// printf("%s\n",strC);
+	while(fscanf(ifp,"%s", output) == 1)
+	{
+		// free(output);
+		// output = (char *)malloc(sizeof(char))
+		
+		printf("%d\n", (int)(strlen(output)));
 
-	// printf("%s\n",output );
+		printf("end of string at %d = %d\n",(int)(strlen(output)),output[strlen(output)] == '\0' );
+
+		flipped = flip_url(output);
+		printf("%s\n",flipped );
+		flipped = fix_errors(flipped);
+		fprintf(ofp, "%s\n",  flipped);
+	};
 
 	return 0;
+
+}
+
+
+
+
+
+
+
+char * fix_errors(char * url)
+{
+	char * test = url;
+	char * result = (char *)malloc(sizeof(char) * (strlen(url) + 1));
+
+	int i = 0;
+
+	while(test[i] != '\0')
+	{
+		i++;
+	};
+
+
+
+	return result;
+
+}
+
+
+
+
+char * flip_url(char * url)
+{
+	int length = strlen(url);
+	char * host, * top_level_domain, * end_of_string, * middle, * filepath, * result, * test = url;
+	char * new_host = (char *)malloc(length*sizeof(char));
+	int tld_size, last_letter_of_host, i, middle_size, hostname_size;
+	char * hostname;
+
+
+
+
+	//search for either the '\0' string terminator or the first / to start the path portion
+	// of the url
+
+	do 
+	{
+		test = strchr(test + sizeof(char),'/');
+		// printf("test = %s\n", test);
+		// sleep(1);
+	}while ((test != NULL) && (((test[-1] == '/') || (test[1] == '/'))));
+
+	result = test;
+
+	test = url;
+
+	if( strchr(&(strchr(url, '.')[1]),'.') == NULL )
+	{
+		//there was only one dot in the url, meaning that we have something like
+		// http://google.com/
+
+		host = (char *)malloc(3*sizeof(char));
+		host[0] = host[1] = host[2] = 'w';
+		hostname_size = 3;
+
+		if(result == NULL)
+		{
+			end_of_string = strchr(url,'\0');
+
+			top_level_domain = &((strchr(url,'.'))[1]);
+
+			// printf("tld = %s\n", top_level_domain);
+
+			tld_size = end_of_string - top_level_domain;
+
+		}
+		else
+		{
+			do
+			{
+				// printf("here\n");
+				//find the last dot before the path. this immediately precedes the top level domain.
+				test = strchr(&(test[1]),'.');
+				// printf("test = %s\n", test);
+			}
+			while((strchr(&(test[1]),'.') != NULL) && (strchr(&(test[1]),'.') - result < 0));
+
+			top_level_domain = &(test[1]);
+
+			// printf("tld = %s\n", top_level_domain);
+
+			// find the tld length
+			tld_size = result - top_level_domain;
+
+			test = url;
+		}
+
+		do 
+		{
+			test = strchr(test + sizeof(char),'/');
+			// printf("test = %s\n", test);
+			// sleep(1);
+		}while (test[-1] != '/');
+
+		test = &(test[1]); //starts with the hostname
+
+		middle = (char *)malloc((length+1)*sizeof(char));
+		middle[length] = '\0';
+		middle[0] = '.';
+		strcpy(middle + sizeof(char), test);
+		middle_size = top_level_domain - test + 1;
+
+		// printf("middle = %s\n", middle);
+
+		// printf("hostname_size = %d, tld_size = %d, middle_size = %d\n", hostname_size,tld_size,middle_size);
+
+		strncpy(new_host, top_level_domain, tld_size);
+		// printf("new_host = %s\n", new_host );
+		strncpy(new_host + sizeof(char)*tld_size,middle,middle_size);
+		// printf("new_host = %s\n", new_host );
+		strncpy(new_host + sizeof(char)*(tld_size+middle_size),host,hostname_size);
+		// printf("new_host = %s\n", new_host );
+
+		if(result !=NULL)
+		{
+			strcpy(new_host + sizeof(char)*(tld_size+middle_size+hostname_size), result);
+			// printf("new_host = %s\n", new_host );
+		}
+
+
+
+
+		return new_host;
+	}
+
+
+
+
+
+
+
+	if ( result == NULL)
+	{
+		// printf("the test was null\n");
+		// there was no filepath, just the main site
+		end_of_string = strchr(url,'\0');
+		test = url;
+		do
+		{
+			//find the last dot. this immediately precedes the top level domain.
+			test = strchr(&(test[1]),'.');
+		}
+		while(strchr(&(test[1]),'.') != NULL);
+
+		top_level_domain = &(test[1]);
+		// printf("tld = %s\n", top_level_domain);
+
+		// find the tld length
+		tld_size = end_of_string - top_level_domain;
+
+		test = url;
+
+		do
+		{	
+			//find the second last dot. this is what seperates the hostname
+			//from the remainder of the host
+			test = strchr(&(test[1]),'.');
+		}while( strchr(&(strchr(&(test[1]),'.')[1]),'.') != NULL);
+
+		middle = test;
+		middle_size = top_level_domain - middle;
+		// printf("middle = %s\n", middle);
+	}
+	else
+	{
+		// here we do have a filepath at the end and test points to the beginning of this
+		// filepath, i.e. the first '/' of the path
+
+		filepath = result;
+
+		test = url;
+
+		do
+		{
+			// printf("here\n");
+			//find the last dot before the path. this immediately precedes the top level domain.
+			test = strchr(&(test[1]),'.');
+			// printf("test = %s\n", test);
+		}
+		while((strchr(&(test[1]),'.') - filepath < 0) && (strchr(&(test[1]),'.') != NULL));
+
+		top_level_domain = &(test[1]);
+
+		// printf("tld = %s\n", top_level_domain);
+
+		// find the tld length
+		tld_size = filepath - top_level_domain;
+
+		test = url;
+
+		do
+		{	
+			//find the second last dot. this is what seperates the hostname
+			//from the remainder of the host
+			test = strchr(&(test[1]),'.');
+		}while( (strchr(&(strchr(&(test[1]),'.')[1]),'.') - filepath < 0) && (strchr(&(strchr(&(test[1]),'.')[1]),'.') != NULL));
+
+		middle = test;
+
+		// printf("middle = %s\n",middle );
+		middle_size = top_level_domain - middle;
+	}
+
+	test = url;
+	do 
+	{
+		test = strchr(test + sizeof(char),'/');
+		// printf("test = %s\n", test);
+		// sleep(1);
+	}while (test[-1] != '/');
+
+	host = test + sizeof(char);
+
+	// printf("host = %s\n",host );
+
+	hostname_size = middle - host;
+
+	// printf("hostname_size = %d, tld_size = %d, middle_size = %d\n", hostname_size,tld_size,middle_size);
+
+	strncpy(new_host, top_level_domain, tld_size);
+	// printf("new_host = %s\n", new_host );
+	strncpy(new_host + sizeof(char)*tld_size,middle,middle_size);
+	// printf("new_host = %s\n", new_host );
+	strncpy(new_host + sizeof(char)*(tld_size+middle_size),host,hostname_size);
+	// printf("new_host = %s\n", new_host );
+
+	if(result !=NULL)
+	{
+		strcpy(new_host + sizeof(char)*(tld_size+middle_size+hostname_size), filepath);
+		// printf("new_host = %s\n", new_host );
+	}
+
+	// printf("%s\n",test);
+
+	return new_host;
 
 }
